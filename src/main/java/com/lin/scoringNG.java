2ec61be46bf2d58b379386d8822320633b21033a
andylin9611052 <andylin9611052@gmail.com>
2022-03-04 15:42:22 +0800

    scoringNG

--- src/main/java/com/lin/scoringNG.java
+++ src/main/java/com/lin/scoringNG.java
@@ -6,7 +6,7 @@ public class scoringNG {
         int [] maths ={80,70,60,50,40};
         int [] englishs = {90,80,70,70,50,};
         for (int i = 0; i < 5; i++) {
-            System.out.println(names[i] + "\t" + maths[i] + "\t" + englishs[i]);
+            System.out.println(names[i] + "\t" + maths[i] + "\t" + englishs[i] +"\t"+ (maths[i] + englishs[i])/2);
         }
     }
 }
