d69271b52ea3b4968c65319f014108706a991c5d
andylin9611052 <andylin9611052@gmail.com>
2022-04-02 23:25:43 +0800

    20220402

--- src/main/java/com/lin/sales/SalesTester.java
+++ src/main/java/com/lin/sales/SalesTester.java
@@ -1,8 +1,11 @@
 package com.lin.sales;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class SalesTester {
     public static void main(String[] args) {
-        Customer c1 =new Customer ("0001",1200);
+        /*Customer c1 =new Customer ("0001",1200);
         Customer c2 =new Customer ("0002",8000);
         SilverCustomer s1 = new SilverCustomer("0001",1200);
         GoldenCustomer g1 = new GoldenCustomer("0001" , 4000);
@@ -11,6 +14,17 @@ public class SalesTester {
         c2.print();
         s1.print();
         g1.print();
-        p1.print();
+        p1.print();*/
+        List<Customer> customers = new ArrayList<>();
+        customers.add(new Customer ("0001",1200));
+        customers.add(new Customer ("0002",8000));
+        customers.add(new SilverCustomer("0001",1200));
+        customers.add(new GoldenCustomer("0001" , 4000));
+        customers.add(new PlatinumCustomer("0001" , 6000));
+
+        for (Customer c: customers) {
+            c.print();
+        }
     }
+
 }
--- src/main/java/com/lin/score/Scoring.java
+++ src/main/java/com/lin/score/Scoring.java
@@ -1,10 +1,28 @@
 package com.lin.score;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Scoring {
     public static void main(String[] args) {
-        Student jack = new Student();
-        jack.name = "Jack";
-        jack.english = 90;
-        jack.math = 80;
+       /* Student jack = new Student("Jack",70,88);
+//        jack.name = "Jack";
+        //jack.english = 70;
+        //jack.math = 88;
+        Student hack = new Student("Hank",60,30);
+        Student s = new Student();
+        GraduateStudent jane = new GraduateStudent("Jane", 70, 80, 60);
+        jane.name = "Jane";
+        jane.print();
+        jack.print();
+        hack.print();
+        */
+        List<Student> students = new ArrayList<>();
+        students.add(new Student("Jack",70,88););
+        students.add(new Student("Hank",60,30));
+        students.add(new GraduateStudent("Jane", 70, 80, 60));
+        for (Student c: students) {
+            c.print();
+        }
     }
 }
