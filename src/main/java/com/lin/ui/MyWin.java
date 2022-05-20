b4c8206fb9b273a50ecec059065c59f170a3a43f
andylin9611052 <andylin9611052@gmail.com>
2022-05-20 16:25:13 +0800

    ui

---
+++ src/main/java/com/lin/ui/MyWin.java
@@ -0,0 +1,35 @@
+package com.lin.ui;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class MyWin {
+    public static void main(String[] args) {
+        JFrame frame  = new JFrame();
+        //big or small
+        frame.setSize(600,400);
+        //locate
+        frame.setLocation(300,200);
+        //close the frame --> end
+        frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        //add Component
+        JButton button = new JButton("OK!");
+        //Anonymous class
+        button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("hahaha");
+            }
+        });
+            //   MyActionListener listener = new MyActionListener();
+            //   button.addActionListener(listener);
+                frame.setLayout(new FlowLayout());
+        frame.add(button);
+        frame.add(new JButton("Testing"));
+        frame.setVisible(true);
+
+        System.out.println("END?");
+    }
+}
